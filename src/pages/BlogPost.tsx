7e45089a15eeeb6bc70262ccb7cdd4761ac6e6cf
Hoa Truong <duchoa201093@gmail.com>
2025-07-30 13:43:19 +0700

    Initial commit: Portfolio with AI server

---
+++ src/pages/BlogPost.tsx
@@ -0,0 +1,13 @@
+import BlogHeader from "@/components/blog/BlogHeader";
+import BlogContent from "@/components/blog/BlogContent";
+
+const BlogPost = () => {
+  return (
+    <div className="min-h-screen">
+      <BlogHeader />
+      <BlogContent />
+    </div>
+  );
+};
+
+export default BlogPost;
